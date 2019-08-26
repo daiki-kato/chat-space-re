3c7b0cc22558fdf41cdf57f3450980bfb19d0441
daiki-kato <53987124+daiki-kato@users.noreply.github.com>
2019-08-26 17:09:06 +0900

    asynch-message-finished

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,56 @@
+$(document).on('turbolinks:load', function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="upper-message">
+                    <p class="upper-message__user-name">
+                      ${message.user_name}
+                    </p>
+                    <p class="upper-message__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      ${content}
+                    </p>
+                    <p class="lower-message__image">
+                      ${img}
+                    </p>
+                </div>`
+  return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var message = new FormData(this);
+    var url = (window.location.href);
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: message,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      function scrollBottom(){
+        var target = $('.message').last();
+        var position = target.offset().top + $('.messages').scrollTop();
+        $('.messages').animate({
+          scrollTop: position
+        }, 300, 'swing');
+      }
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_content').val('');
+      scrollBottom();
+    })
+    .fail(function(data){
+      alert('エラーが発生したためメッセージは送信できませんでした。');
+    })
+    .always(function(data){
+      $('.form__submit').prop('disabled', false);
+    })
+  })
+});
