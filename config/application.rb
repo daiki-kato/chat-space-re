bb928ed5e0a9bf1bdbb08b517c9d7a2ea8701240
daiki-kato <53987124+daiki-kato@users.noreply.github.com>
2019-08-31 19:04:14 +0900

    test_back

--- config/application.rb
+++ config/application.rb
@@ -13,9 +13,9 @@ module ChatSpaceRe
       g.javascripts false
       g.helper false
       g.test_framework false
+      config.i18n.default_locale = :ja
+      # config.time_zone = 'Tokyo'
     end
-    config.i18n.default_locale = :ja
-    config.time_zone = 'Tokyo'
   end
 end
 
